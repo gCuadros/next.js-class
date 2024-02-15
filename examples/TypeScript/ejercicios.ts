6030850f14b15b0ac06ce0fc590d352b998aa929
José Miguel <Tapuy@hotmail.es>
2024-02-15 19:29:00 +0100

    add second interface

--- examples/TypeScript/ejercicios.ts
+++ examples/TypeScript/ejercicios.ts
@@ -6,12 +6,38 @@ function saluda(nombre) {
   return `Hola, ${nombre}!`
 }
 
+interface Persona  { // se hace la interface y encima del objeto al que se hace referencia
+  nombre: string,
+  apellido: string,
+  estadoCivil: string,
+  edad: number,
+  esEstudiante: true
+}
+
 const persona = {
   nombre: 'Alice',
   apellido: 'Smith',
   estadoCivil: 'soltera',
   edad: 30,
   esEstudiante: true
+}
+
+// interface Estudiante {
+//   nombre: string,
+//   apellido: string,
+//   estadoCivil: string,
+//   edad: number,
+//   universidad: string,
+//   matricula: string,
+//   curso: number,
+//   clase : string,
+// }
+
+interface Estudiante extends Persona {
+universidad: string,
+matricula: string,
+curso: number,
+clase : string,
 }
 
 const estudiante = {
